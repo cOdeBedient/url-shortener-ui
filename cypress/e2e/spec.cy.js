720b9215374fb708ecb49d55511c005cb7abd6d7
Adam Bedient <adambedient@gmail.com>
2024-04-22 12:09:52 -0400

    Validate all items on page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -26,6 +26,17 @@ describe('empty spec', () => {
       }
     })
     cy.visit('http://localhost:3000/')
-
+      .get('h1').contains('URL Shortener')
+      .get('form')
+      .contains('button', 'Shorten Please!')
+      .get("input[name='Title']")
+      .get("input[name='url']")
+      .get('section').children().should('have.length', 3)
+      .get("section").children().first().contains('h3', 'Awesome photo')
+      .get("section").children().first().contains('a', 'http://localhost:3001/useshorturl/1')
+      .get("section").children().first().contains('p', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+      .get("section").children().last().contains('h3', 'Cluuuutch')
+      .get("section").children().last().contains('a', 'http://localhost:3001/useshorturl/3')
+      .get("section").children().last().contains('p', 'https://unsplash.com/photos/a-woman-holding-a-white-purse-on-the-street-uhWdD5OMQNg') 
   })
 })
